2778f1fb40ecbfa173f4d83910b8afaedefd5d5f
Edgar Martinez <edgarmartinezme@gmail.com>
2020-04-10 10:38:00 -0600

    Corregido comando para cambio de directorio

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -3,4 +3,4 @@ sudo apt update -y
 sudo apt install curl git zip unzip -y
 sudo curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
-sudo cd /var/dev/getdora.app
+cd /var/dev/getdora.app
